50b6157f5d56526d3fe42a0c7bfa80597ee4fe00
Hatem Nabli <hatem.nabli@actemium.com>
2024-04-09 22:23:55 +0200

    Feat: on encoding method, replace invalid code points with replacement character

--- src/Utf8.cpp
+++ src/Utf8.cpp
@@ -9,6 +9,26 @@
 #include <Utf8\Utf8.hpp>
 namespace {
 
+    /**
+     * This is the Unicode replacement character encoded as Utf8.
+    */
+    const std::vector< Utf8::UnicodeCodePoint > UTF8_ENCODED_REPLACEMENT_CHAR = {
+        0xEF, 0xBF, 0xBD 
+    };
+
+    /**
+     * This is the very last code point in Unicode tha is legal.
+    */
+    const Utf8::UnicodeCodePoint LAST_LEGAL_UNICODE_CODE_POINT = 0x10FFFF;
+    /**
+     * the hight and low surrogate halves used by UTF-16
+     * (U+D800 through U+DFFF) and code points not encodable
+     * by UTF-16 (those after U+10FFFF) are not legal Unicode values, and
+     * their UTF-8 encoding must be treated as an invalid byte sequence.
+    */
+    const Utf8::UnicodeCodePoint FIRST_SURROGATE = 0xD800;
+    const Utf8::UnicodeCodePoint LAST_SURROGATE = 0xDFFF;
+
     template< typename I > size_t log2n(I integer) {
         size_t n = 0;
         while (integer > 0) {
@@ -50,14 +70,31 @@ namespace Utf8 {
                 encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x1F) + 0xC0));
                 encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
             } else if (numBits <= 16) {
-                encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x0F) + 0xE0));
-                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F) + 0x80));
-                encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
-            } else if (numBits <= 21) {
-                encoding.push_back((UnicodeCodePoint)(((codePoint >> 18) & 0x07) + 0xf0));
+                if (
+                    (codePoint >= FIRST_SURROGATE)
+                    && (codePoint <= LAST_SURROGATE)
+                ) {
+                    encoding.insert(
+                        encoding.end(),
+                        UTF8_ENCODED_REPLACEMENT_CHAR.begin(),
+                        UTF8_ENCODED_REPLACEMENT_CHAR.end()
+                    );
+                } else {
+                    encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x0F) + 0xE0));
+                    encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F) + 0x80));
+                    encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
+                }
+            } else if ((numBits <= 21) && (codePoint <= LAST_LEGAL_UNICODE_CODE_POINT)) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 18) & 0x07) + 0xF0));
                 encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x3F) + 0x80));
                 encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F) + 0x80));
                 encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
+            } else {
+                encoding.insert(
+                    encoding.end(),
+                    UTF8_ENCODED_REPLACEMENT_CHAR.begin(),
+                    UTF8_ENCODED_REPLACEMENT_CHAR.end()
+                );
             }
             
         }
