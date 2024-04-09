b074470525f114684d3f6b80a2ba1308d45e9460
Hatem Nabli <hatem.nabli@actemium.com>
2024-04-09 18:00:10 +0200

    Test: test support for valid non-ASCII Unicode

--- test/src/Utf8Tests.cpp
+++ test/src/Utf8Tests.cpp
@@ -25,3 +25,24 @@ TEST(Utf8Tests, EncodeAscii) {
     const auto actualEncoding = utf8.Encode(Utf8::AsciiToUnicode("Hello"));
     ASSERT_EQ(expectedEncoding, actualEncoding);
 }
+
+TEST(Utf8Tests, Symbols) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0x41, 0xE2, 0x89, 0xA2, 0xCE, 0x91, 0x2E };
+    const auto actualEncoding = utf8.Encode({0x0041, 0x2262, 0x0391, 0x002E});
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
+
+TEST(Utf8Tests, EncodeJapanese_Test) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0xE6, 0x97, 0xA5, 0xE6, 0x9C, 0xAC, 0xE8, 0xAA, 0x9E };
+    const auto actualEncoding = utf8.Encode({0x65E5, 0x672C, 0x8A9E});
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
+
+TEST(Utf8Test, StumpOfTree) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0xF0, 0xA3, 0x8E, 0xB4 };
+    const auto actualEncoding = utf8.Encode({0x233B4});
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
