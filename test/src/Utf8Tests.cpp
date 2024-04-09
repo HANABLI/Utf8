e9b6b6d618b6be66b70fd1968d37386068d2e896
Hatem Nabli <hatem.nabli@actemium.com>
2024-04-09 05:03:09 +0200

    Test: test method supporting ASCII

--- test/src/Utf8Tests.cpp
+++ test/src/Utf8Tests.cpp
@@ -13,3 +13,15 @@ TEST(Utf8Tests, PlaceHolder_Test) {
     Utf8::Utf8 utf8;
     ASSERT_TRUE(true);
 }
+
+TEST(Utf8Tests, AsciiToUnicode_Test) {
+    const std::vector< Utf8::UnicodeCodePoint > expectedEncodingPoints{ 0x48, 0x65, 0x6C, 0x6C, 0x6F };
+    const auto actualCodePoints = Utf8::AsciiToUnicode("Hello");
+}
+
+TEST(Utf8Tests, EncodeAscii) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0x48, 0x65, 0x6C, 0x6C, 0x6F };
+    const auto actualEncoding = utf8.Encode(Utf8::AsciiToUnicode("Hello"));
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
